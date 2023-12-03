e3355e4732e1b7622ba4262ece9cc6d5fe6e8237
szymon-mielewczyk <126406836+szymon-mielewczyk@users.noreply.github.com>
2023-12-03 14:27:05 +0100

    Update request.py

--- request.py
+++ request.py
@@ -1,8 +1,9 @@
 import requests
 
 resp = requests.post("http://localhost:5000/api/ai/predict",
-                     files={"imageId":"0",
+                     json={"imageId":"0",
                             "imageName": "mdb012.pgm"
                             })
+
 
 print(resp.json())
